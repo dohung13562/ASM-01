da70cedc3ab36e497189eb9be2fa92e1922e66a2
dohung13562 <thinhhung1808@gmail.com>
2022-03-12 17:52:04 +0700

    them background

--- src/components/StaffsInfomation.js
+++ src/components/StaffsInfomation.js
@@ -19,14 +19,12 @@ class StaffsInfomation extends Component {
                         <p className='mt-2' style={{ color: "#C0C0C0" }}><Link to='/'>Nhân viên</Link> / {staff.name}</p>
                             <CardImg src={staff.image} />
                                 <Card>
-                                    <div className="ms-1">
-                                        <h4><span className="fa fa-user-circle" />Họ và tên : {staff.name}</h4>
-                                        <p><span className="fa fa-birthday-cake" />Ngày sinh : {dateFormat(staff.doB, "dd/mm/yyyy")}</p>
-                                        <p><span className="fa fa-calendar" />Ngày vào công ty : {dateFormat(staff.startDate, "dd/mm/yyyy")}</p>
-                                        <p><span className="fa fa-sitemap" />Phòng ban : {staff.department.name}</p>
-                                        <p><span className="fa fa-user-times" />Số ngày nghỉ còn lại : {staff.annualLeave}</p>
-                                        <p><span className="fa fa-user-plus" />Số ngày đã làm thêm : {staff.overTime}</p>
-                                    </div>
+                                    <h4><span className="fa fa-user-circle" />Họ và tên : {staff.name}</h4>
+                                    <p><span className="fa fa-birthday-cake" />Ngày sinh : {dateFormat(staff.doB, "dd/mm/yyyy")}</p>
+                                    <p><span className="fa fa-calendar" />Ngày vào công ty : {dateFormat(staff.startDate, "dd/mm/yyyy")}</p>
+                                    <p><span className="fa fa-sitemap" />Phòng ban : {staff.department.name}</p>
+                                    <p><span className="fa fa-user-times" />Số ngày nghỉ còn lại : {staff.annualLeave}</p>
+                                    <p><span className="fa fa-user-plus" />Số ngày đã làm thêm : {staff.overTime}</p>
                                 </Card>
                     </div>
                 );
