d813afd39479cf026b139e0caa33b53f74738e79
dohung13562 <thinhhung1808@gmail.com>
2022-05-22 22:16:38 +0700

    LAB 02.2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import StaffList from './components/StaffListComponent';
 import './App.css';
-import { STAFFS } from './shared/staffs';
 
 class App extends Component {
-
-  constructor(props) {
-     super(props);
-
-     this.state = {
-       staffs: STAFFS,
-     };
-   }
-
   render() {
     return (
       <div className="App">
         <Navbar dark color="primary">
-          <div className='container-fluid'>
-            <NavbarBrand href='/'>Ứng dụng quản lý nhân sự v1.0 </NavbarBrand>
+          <div className='container'>
+            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <StaffList staffs={this.state.staffs} />
       </div>
     );
   }
